ced4d0a10d27fdb54f8fe85168b8cc9a4d6334b7
Zach Philipp <zachphilipp@icloud.com>
2025-06-08 21:34:40 -0500

    install shadcn and cli

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+/*
+  FOR IMAGES
+  1. put in-app images in src/assets
+  2. put title images or direct html images in public
+
+  DEPLOY ACTIONS
+  1. npm run predeploy
+  2. npm run deploy
+  3. wait a few minutes or reset page 'Cmd + Shift + R'
+*/
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
