be7235827e046ee367a67e8761d1fcb63eedb9f1
Zach Philipp <zachphilipp@icloud.com>
2025-06-15 14:44:23 -0500

    added footer and container to main DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import HomeSection from './components/home-section';
 import AboutSection from './components/about-section';
 import SkillsSection from './components/skills-section';
+import Footer from './components/footer';
+import Container from './components/container';
 
 function App() {
   return (
     <>
-      <main className="relative bg-background h-auto w-full">
+      <Container>
         {/* fade in intro on page render with header, profile photo, and description */}
         <HomeSection />
         {/* details more about me and what I can do  */}
         <AboutSection />
         {/* post about what I'm working on and what I work with */}
         <SkillsSection />
-      </main>
+      </Container>
+      <Footer />
     </>
   );
 }
